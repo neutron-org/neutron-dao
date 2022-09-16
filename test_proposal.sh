4bdf56f2b53a5916a83106031ebecd8127b10a52
quasisamurai <quazisamurai@gmail.com>
2022-09-16 12:08:59 +0400

    wip

--- test_proposal.sh
+++ test_proposal.sh
@@ -1,5 +1,6 @@
 BIN=neutrond
 
+CONTRACT=./artifacts/neutron_interchain_queries.wasm
 
 CHAIN_ID_1=test-1
 CHAIN_ID_2=test-2
@@ -16,36 +17,20 @@ ADMIN=$(neutrond keys show demowallet1 -a --keyring-backend test --home ${HOME_1
 TARGET_ADDRESS=neutron1mjk79fjjgpplak5wq838w0yd982gzkyf8fxu8u
 VAL2=neutronvaloper1qnk2n4nlkpw9xfqntladh74w6ujtulwnqshepx
 
-#Register new proposal
-# json formatted proposal is a nightmare, so we use keys for now
-RES=$(${BIN} tx gov submit-proposal --title="hello proposal" \
-  --description="i believe in neutron supremacy!" \
-  --type="Text" \
-  --deposit="100000000stake" \
-  --from ${USERNAME_1} \
-  --gas 500000 \
-  --fees 5000stake \
-  -y \
-  --chain-id ${CHAIN_ID_1} \
-  --broadcast-mode=block \
-  --home ${HOME_1} \
-  --keyring-backend test \
-  --node tcp://127.0.0.1:16657)
+# Upload the queries contract
+RES=$(${BIN} tx wasm store ${CONTRACT} --from ${USERNAME_1} --gas 50000000  --chain-id ${CHAIN_ID_1} --broadcast-mode=block --gas-prices 0.0025stake  -y --output json  --keyring-backend test --home ${HOME_1} --node tcp://127.0.0.1:16657)
+QUERIES_CONTRACT_CODE_ID=$(echo $RES | jq -r '.logs[0].events[1].attributes[0].value')
 echo $RES
-echo
+echo $QUERIES_CONTRACT_CODE_ID
 
-# print proposal in console, dao period should be active
-RES=$(${BIN} q gov proposals  --chain-id ${CHAIN_ID_1}   --home ${HOME_1}   --node tcp://127.0.0.1:16657)
-echo $RES
-echo
+# Instantiate the queries contract
+INIT_QUERIES_CONTRACT='{}'
 
-# vote yes (w dominance of votes)
-RES=$(${BIN} tx gov vote 1 yes --from ${USERNAME_1} --fees 5000stake --chain-id ${CHAIN_ID_1} -y --broadcast-mode=block --home ${HOME_1}  --keyring-backend test --node tcp://127.0.0.1:16657)
+RES=$(${BIN} tx wasm instantiate $QUERIES_CONTRACT_CODE_ID "$INIT_QUERIES_CONTRACT" --from ${USERNAME_1} --admin ${ADMIN} -y --chain-id ${CHAIN_ID_1} --output json --broadcast-mode=block --label "init"  --keyring-backend test --gas-prices 0.0025stake --home ${HOME_1} --node tcp://127.0.0.1:16657)
 echo $RES
-echo
-# wait dao period to end
-sleep 20
-#
-# print  proposal to see that it has passed
-RES=$(${BIN} q gov proposals  --chain-id ${CHAIN_ID_1}   --home ${HOME_1}   --node tcp://127.0.0.1:16657)
+QUERIES_CONTRACT_ADDRESS=$(echo $RES | jq -r '.logs[0].events[0].attributes[0].value')
+echo $QUERIES_CONTRACT_ADDRESS
+
+# Register a query for KEY_2 balance
+RES=$(${BIN} tx wasm execute $QUERIES_CONTRACT_ADDRESS "{\"register_balance_query\": {\"connection_id\": \"connection-0\", \"denom\": \"stake\", \"addr\": \"${KEY_2}\", \"update_period\": 5, \"zone_id\": \"${CHAIN_ID_2}\"}}" --from ${USERNAME_1}  -y --chain-id ${CHAIN_ID_1} --output json --broadcast-mode=block --gas-prices 0.0025stake --gas 1000000 --keyring-backend test --home ${HOME_1} --node tcp://127.0.0.1:16657)
 echo $RES
