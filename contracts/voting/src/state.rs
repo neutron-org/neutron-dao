0b7f9a6dcb80343b05ce0e3cf0192836622bf4c3
quasisamurai <quazisamurai@gmail.com>
2022-11-10 15:01:10 +0400

    dao

--- contracts/voting/src/state.rs
+++
@@ -1,6 +0,0 @@
-use cosmwasm_std::{Addr, Uint128};
-use cw_storage_plus::{Item, Map};
-
-pub const TOKENS_LOCKED: Map<&Addr, Uint128> = Map::new("positions");
-
-pub const OWNER: Item<Addr> = Item::new("owner");
