6693c564f082300a11c70079a3c2095629d8b316
oldremez <83083413+oldremez@users.noreply.github.com>
2022-12-22 00:00:47 +0300

    wip

---
+++ contracts/pre-propose/cwd-pre-propose-single-overrule/src/contract.rs
@@ -0,0 +1,128 @@
+#[cfg(not(feature = "library"))]
+use cosmwasm_std::entry_point;
+use cosmwasm_std::{
+    to_binary, Addr, Binary, CosmosMsg, Deps, DepsMut, Env, MessageInfo, Response, StdResult,
+    WasmMsg,
+};
+use cw2::set_contract_version;
+use neutron_bindings::bindings::msg::NeutronMsg;
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+use cwd_pre_propose_base::{
+    error::PreProposeError,
+    msg::{ExecuteMsg as ExecuteBase, InstantiateMsg as InstantiateBase, QueryMsg as QueryBase},
+    state::PreProposeContract,
+};
+// use cwd_voting::deposit::UncheckedDepositInfo;
+
+pub(crate) const CONTRACT_NAME: &str = "crates.io:cwd-pre-propose-single-timelocked";
+pub(crate) const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
+
+#[derive(Serialize, JsonSchema, Deserialize, Debug, Clone)]
+#[serde(rename_all = "snake_case")]
+pub enum ProposeMessage {
+    ProposeOverrule {
+        timelock_contract: Addr,
+        proposal_id: u64,
+    },
+}
+
+#[derive(Serialize, Deserialize, JsonSchema, Debug, Clone)]
+#[serde(rename_all = "snake_case")]
+pub enum TimelockExecuteMsg {
+    OverruleProposal { proposal_id: u64 },
+}
+
+pub type ExecuteMsg = ExecuteBase<ProposeMessage>;
+pub type QueryMsg = QueryBase;
+
+#[derive(Serialize, Deserialize, Clone, Debug, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub struct InstantiateMsg {}
+
+/// Internal version of the propose message that includes the
+/// `proposer` field. The module will fill this in based on the sender
+/// of the external message.
+#[derive(Serialize, JsonSchema, Deserialize, Debug, Clone)]
+#[serde(rename_all = "snake_case")]
+enum ProposeMessageInternal {
+    Propose {
+        title: String,
+        description: String,
+        msgs: Vec<CosmosMsg<NeutronMsg>>,
+        proposer: Option<String>,
+    },
+}
+
+type PrePropose = PreProposeContract<ProposeMessageInternal>;
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn instantiate(
+    mut deps: DepsMut,
+    env: Env,
+    info: MessageInfo,
+    _msg: InstantiateMsg,
+) -> Result<Response, PreProposeError> {
+    // the contract has no info for instantiation, so it just calls the init function of base
+    // deposit is set to zero because it makes no sense for overrule proposals
+    // for open submission it's tbd
+    let resp = PrePropose::default().instantiate(
+        deps.branch(),
+        env,
+        info,
+        InstantiateBase {
+            deposit_info: Option::None,
+            open_proposal_submission: false,
+        },
+    )?;
+    set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
+    Ok(resp)
+}
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn execute(
+    deps: DepsMut,
+    env: Env,
+    info: MessageInfo,
+    msg: ExecuteMsg,
+) -> Result<Response, PreProposeError> {
+    // We don't want to expose the `proposer` field on the propose
+    // message externally as that is to be set by this module. Here,
+    // we transform an external message which omits that field into an
+    // internal message which sets it.
+    type ExecuteInternal = ExecuteBase<ProposeMessageInternal>;
+    let internalized = match msg {
+        ExecuteMsg::Propose {
+            msg:
+                ProposeMessage::ProposeOverrule {
+                    timelock_contract,
+                    proposal_id,
+                },
+        } => {
+            let overrule_msg = CosmosMsg::Wasm(WasmMsg::Execute {
+                contract_addr: timelock_contract.to_string(),
+                msg: to_binary(&TimelockExecuteMsg::OverruleProposal { proposal_id }).unwrap(),
+                funds: vec![],
+            });
+
+            ExecuteInternal::Propose {
+                msg: ProposeMessageInternal::Propose {
+                    // Fill in proposer based on message sender.
+                    proposer: Some(info.sender.to_string()),
+                    title: "Overrule proposal _number_ of _subdao_name_ subdao".to_string(),
+                    description: "Reject the decision made by subdao".to_string(),
+                    msgs: vec![overrule_msg],
+                },
+            }
+        },
+        _ => panic!(""),
+    };
+
+    PrePropose::default().execute(deps, env, info, internalized)
+}
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn query(deps: Deps, env: Env, msg: QueryMsg) -> StdResult<Binary> {
+    PrePropose::default().query(deps, env, msg)
+}
