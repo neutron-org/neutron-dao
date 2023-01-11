92ba326a8e551720193bf13a9523ba8d9d1557c3
oldremez <83083413+oldremez@users.noreply.github.com>
2023-01-11 15:34:59 +0200

    wip

---
+++ contracts/dao/subdao-registry/README.md
@@ -0,0 +1,1 @@
+# Neutron subdao registry
---
+++ contracts/dao/subdao-registry/src/state.rs
@@ -0,0 +1,26 @@
+use cosmwasm_std::{Addr, Decimal, Uint128};
+use cw_storage_plus::Item;
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
+pub struct Config {
+    /// Distribution rate (0-1) which goes to distribution contract
+    pub distribution_rate: Decimal,
+    /// Address of distribution contract, which will receive funds defined but distribution_rate %
+    pub distribution_contract: Addr,
+    /// Address of reserve contract, which will receive funds defined by 100-distribution_rate %
+    pub reserve_contract: Addr,
+    /// Minimum period between distribution calls
+    pub min_period: u64,
+    pub denom: String,
+    pub owner: Addr,
+}
+
+pub const TOTAL_RECEIVED: Item<Uint128> = Item::new("total_received");
+pub const TOTAL_DISTRIBUTED: Item<Uint128> = Item::new("total_distributed");
+pub const TOTAL_RESERVED: Item<Uint128> = Item::new("total_reserved");
+
+pub const LAST_DISTRIBUTION_TIME: Item<u64> = Item::new("last_grab_time");
+
+pub const CONFIG: Item<Config> = Item::new("config");
