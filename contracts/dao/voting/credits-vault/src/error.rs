285c2ddaf5c904d5b88fd3e128732e6950b6e136
Albert Andrejev <albert.andrejev@gmail.com>
2023-02-28 17:14:18 +0200

    remove not used code

--- contracts/dao/voting/credits-vault/src/error.rs
+++ contracts/dao/voting/credits-vault/src/error.rs
@@ -1,5 +1,4 @@
 use cosmwasm_std::StdError;
-use cw_utils::PaymentError;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
@@ -7,18 +6,9 @@ pub enum ContractError {
     #[error("{0}")]
     Std(#[from] StdError),
 
-    #[error("{0}")]
-    PaymentError(#[from] PaymentError),
-
     #[error("Unauthorized")]
     Unauthorized {},
 
-    #[error("Nothing to claim")]
-    NothingToClaim {},
-
     #[error("Only owner can change owner")]
     OnlyOwnerCanChangeOwner {},
-
-    #[error("Can only unbond less than or equal to the amount you have bonded")]
-    InvalidUnbondAmount {},
 }
