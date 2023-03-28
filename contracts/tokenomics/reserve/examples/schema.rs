0792be7ebcf630cd6b2eabffdc1ba7d9b87407a0
quasisamurai <quazisamurai@gmail.com>
2023-03-28 11:20:01 +0400

    schema fix

--- contracts/tokenomics/reserve/examples/schema.rs
+++ contracts/tokenomics/reserve/examples/schema.rs
@@ -17,7 +17,7 @@ use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
-use neutron_treasury::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use neutron_reserve::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
