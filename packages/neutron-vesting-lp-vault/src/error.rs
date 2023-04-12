a28d92ddf470e3fcdd5c018bc01691412d355c09
Murad Karammaev <muradkarammaev@gmail.com>
2023-04-12 19:14:03 +0300

    feat: disable `ListBonders` query

--- packages/neutron-vesting-lp-vault/src/error.rs
+++ packages/neutron-vesting-lp-vault/src/error.rs
@@ -1,10 +1,13 @@
-use cosmwasm_std::StdError;
+use cosmwasm_std::{ConversionOverflowError, StdError};
 use thiserror::Error;
 
 #[derive(Error, Debug, PartialEq)]
 pub enum ContractError {
     #[error("{0}")]
     Std(#[from] StdError),
+
+    #[error("{0}")]
+    ConversionOverflowError(#[from] ConversionOverflowError),
 
     #[error("Unauthorized")]
     Unauthorized {},
