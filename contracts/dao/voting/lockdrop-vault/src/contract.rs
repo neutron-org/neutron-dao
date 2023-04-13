1c828454f1bd28989a329fe90817cbe57182222b
Murad Karammaev <muradkarammaev@gmail.com>
2023-04-13 11:03:56 +0300

    feat: disable `ListBonders` query for lockdrop-vault, too

--- contracts/dao/voting/lockdrop-vault/src/contract.rs
+++ contracts/dao/voting/lockdrop-vault/src/contract.rs
@@ -1,7 +1,7 @@
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
 use cosmwasm_std::{
-    to_binary, Binary, Deps, DepsMut, Env, Fraction, MessageInfo, Response, StdResult, Uint128,
+    to_binary, Binary, Deps, DepsMut, Env, Fraction, MessageInfo, Response, Uint128,
 };
 use cw2::set_contract_version;
 use cwd_interface::voting::{
@@ -13,8 +13,7 @@ use neutron_lockdrop_vault::voting_power::{get_voting_power_for_address, get_vot
 use crate::state::{CONFIG, DAO};
 
 use astroport_periphery::lockdrop::PoolType;
-use cwd_voting::vault::ListBondersResponse;
-use neutron_lockdrop_vault::error::ContractError;
+use neutron_lockdrop_vault::error::{ContractError, ContractResult};
 use neutron_lockdrop_vault::msg::{ExecuteMsg, InstantiateMsg, MigrateMsg, QueryMsg};
 use neutron_lockdrop_vault::types::Config;
 
@@ -27,7 +26,7 @@ pub fn instantiate(
     _env: Env,
     info: MessageInfo,
     msg: InstantiateMsg,
-) -> Result<Response, ContractError> {
+) -> ContractResult<Response> {
     set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
 
     let owner = match msg.owner {
@@ -75,7 +74,7 @@ pub fn execute(
     env: Env,
     info: MessageInfo,
     msg: ExecuteMsg,
-) -> Result<Response, ContractError> {
+) -> ContractResult<Response> {
     match msg {
         ExecuteMsg::Bond {} => execute_bond(deps, env, info),
         ExecuteMsg::Unbond { amount } => execute_unbond(deps, env, info, amount),
@@ -101,11 +100,7 @@ pub fn execute(
     }
 }
 
-pub fn execute_bond(
-    _deps: DepsMut,
-    _env: Env,
-    _info: MessageInfo,
-) -> Result<Response, ContractError> {
+pub fn execute_bond(_deps: DepsMut, _env: Env, _info: MessageInfo) -> ContractResult<Response> {
     Err(ContractError::BondingDisabled {})
 }
 
@@ -114,7 +109,7 @@ pub fn execute_unbond(
     _env: Env,
     _info: MessageInfo,
     _amount: Uint128,
-) -> Result<Response, ContractError> {
+) -> ContractResult<Response> {
     Err(ContractError::DirectUnbondingDisabled {})
 }
 
@@ -129,7 +124,7 @@ pub fn execute_update_config(
     new_manager: Option<String>,
     new_name: Option<String>,
     new_description: Option<String>,
-) -> Result<Response, ContractError> {
+) -> ContractResult<Response> {
     let mut config: Config = CONFIG.load(deps.storage)?;
     if info.sender != config.owner && Some(info.sender.clone()) != config.manager {
         return Err(ContractError::Unauthorized {});
@@ -216,13 +211,13 @@ pub fn execute_update_config(
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
-pub fn query(deps: Deps, env: Env, msg: QueryMsg) -> StdResult<Binary> {
+pub fn query(deps: Deps, env: Env, msg: QueryMsg) -> ContractResult<Binary> {
     match msg {
-        QueryMsg::VotingPowerAtHeight { address, height } => {
-            to_binary(&query_voting_power_at_height(deps, env, address, height)?)
-        }
+        QueryMsg::VotingPowerAtHeight { address, height } => Ok(to_binary(
+            &query_voting_power_at_height(deps, env, address, height)?,
+        )?),
         QueryMsg::TotalPowerAtHeight { height } => {
-            to_binary(&query_total_power_at_height(deps, env, height)?)
+            Ok(to_binary(&query_total_power_at_height(deps, env, height)?)?)
         }
         QueryMsg::Info {} => query_info(deps),
         QueryMsg::Dao {} => query_dao(deps),
@@ -232,9 +227,9 @@ pub fn query(deps: Deps, env: Env, msg: QueryMsg) -> StdResult<Binary> {
         QueryMsg::ListBonders { start_after, limit } => {
             query_list_bonders(deps, start_after, limit)
         }
-        QueryMsg::BondingStatus { height, address } => {
-            to_binary(&query_bonding_status(deps, env, height, address)?)
-        }
+        QueryMsg::BondingStatus { height, address } => Ok(to_binary(&query_bonding_status(
+            deps, env, height, address,
+        )?)?),
     }
 }
 
@@ -243,7 +238,7 @@ pub fn query_voting_power_at_height(
     env: Env,
     address: String,
     height: Option<u64>,
-) -> StdResult<VotingPowerAtHeightResponse> {
+) -> ContractResult<VotingPowerAtHeightResponse> {
     let config = CONFIG.load(deps.storage)?;
 
     let height = height.unwrap_or(env.block.height);
@@ -279,7 +274,7 @@ pub fn query_total_power_at_height(
     deps: Deps,
     env: Env,
     height: Option<u64>,
-) -> StdResult<TotalPowerAtHeightResponse> {
+) -> ContractResult<TotalPowerAtHeightResponse> {
     let config = CONFIG.load(deps.storage)?;
 
     let height = height.unwrap_or(env.block.height);
@@ -309,56 +304,50 @@ pub fn query_total_power_at_height(
     })
 }
 
-pub fn query_info(deps: Deps) -> StdResult<Binary> {
+pub fn query_info(deps: Deps) -> ContractResult<Binary> {
     let info = cw2::get_contract_version(deps.storage)?;
-    to_binary(&cwd_interface::voting::InfoResponse { info })
+    Ok(to_binary(&cwd_interface::voting::InfoResponse { info })?)
 }
 
-pub fn query_dao(deps: Deps) -> StdResult<Binary> {
+pub fn query_dao(deps: Deps) -> ContractResult<Binary> {
     let dao = DAO.load(deps.storage)?;
-    to_binary(&dao)
+    Ok(to_binary(&dao)?)
 }
 
-pub fn query_name(deps: Deps) -> StdResult<Binary> {
+pub fn query_name(deps: Deps) -> ContractResult<Binary> {
     let config = CONFIG.load(deps.storage)?;
-    to_binary(&config.name)
+    Ok(to_binary(&config.name)?)
 }
 
-pub fn query_description(deps: Deps) -> StdResult<Binary> {
+pub fn query_description(deps: Deps) -> ContractResult<Binary> {
     let config = CONFIG.load(deps.storage)?;
-    to_binary(&config.description)
+    Ok(to_binary(&config.description)?)
 }
 
-pub fn query_config(deps: Deps) -> StdResult<Binary> {
+pub fn query_config(deps: Deps) -> ContractResult<Binary> {
     let config = CONFIG.load(deps.storage)?;
-    to_binary(&config)
+    Ok(to_binary(&config)?)
 }
 
 pub fn query_list_bonders(
     _deps: Deps,
     _start_after: Option<String>,
     _limit: Option<u32>,
-) -> StdResult<Binary> {
-    // TODO: implement once the lockdrop contract is implemented.
-    to_binary(&ListBondersResponse { bonders: vec![] })
+) -> ContractResult<Binary> {
+    Err(ContractError::BondingDisabled {})
 }
 
 pub fn query_bonding_status(
     _deps: Deps,
-    env: Env,
-    height: Option<u64>,
+    _env: Env,
+    _height: Option<u64>,
     _address: String,
-) -> StdResult<BondingStatusResponse> {
-    let height = height.unwrap_or(env.block.height);
-    Ok(BondingStatusResponse {
-        unbondable_abount: Uint128::zero(),
-        bonding_enabled: false,
-        height,
-    })
+) -> ContractResult<BondingStatusResponse> {
+    Err(ContractError::BondingDisabled {})
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
-pub fn migrate(deps: DepsMut, _env: Env, _msg: MigrateMsg) -> Result<Response, ContractError> {
+pub fn migrate(deps: DepsMut, _env: Env, _msg: MigrateMsg) -> ContractResult<Response> {
     set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
     Ok(Response::default())
 }
