2308694257e735a42854436432bda1b7d230e67d
Sergey Ratiashvili <serg.s.r@gmail.com>
2023-08-15 18:18:30 +0200

    feat: investors-vesting-vault new schema

--- contracts/dao/voting/investors-vesting-vault/examples/schema.rs
+++ contracts/dao/voting/investors-vesting-vault/examples/schema.rs
@@ -12,20 +12,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::env::current_dir;
-use std::fs::create_dir_all;
-
-use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
-
+use cosmwasm_schema::write_api;
 use investors_vesting_vault::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
 
 fn main() {
-    let mut out_dir = current_dir().unwrap();
-    out_dir.push("schema");
-    create_dir_all(&out_dir).unwrap();
-    remove_schemas(&out_dir).unwrap();
-
-    export_schema(&schema_for!(InstantiateMsg), &out_dir);
-    export_schema(&schema_for!(ExecuteMsg), &out_dir);
-    export_schema(&schema_for!(QueryMsg), &out_dir);
+    write_api! {
+        instantiate: InstantiateMsg,
+        query: QueryMsg,
+        execute: ExecuteMsg,
+    }
 }
