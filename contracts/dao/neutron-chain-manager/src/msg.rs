66d99590f28e841eb206f6217becf677a5279e38
swelf <golishkin@gmail.com>
2024-04-19 02:48:00 +0300

    snake case serialisation

--- contracts/dao/neutron-chain-manager/src/msg.rs
+++ contracts/dao/neutron-chain-manager/src/msg.rs
@@ -6,14 +6,14 @@ use serde::{Deserialize, Serialize};
 
 use crate::permission::Permission;
 
-#[derive(Serialize, Deserialize, JsonSchema, Debug, Clone)]
+#[cw_serde]
+#[serde(rename_all = "snake_case")]
 pub struct InstantiateMsg {
     /// Defines the address for the initial setup.
     pub initial_address: Addr,
 }
 
-#[derive(Serialize, Deserialize, JsonSchema, Debug, Clone)]
-#[serde(rename_all = "snake_case")]
+#[cw_serde]
 pub enum ExecuteMsg {
     AddPermissions {
         address: Addr,
@@ -33,33 +33,37 @@ pub enum QueryMsg {
     #[returns(Vec < Permission >)]
     Permissions {},
 }
-
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
+#[cw_serde]
+#[serde(rename_all = "snake_case")]
 pub struct MigrateMsg {}
-
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
+#[cw_serde]
+#[serde(rename_all = "snake_case")]
 pub struct ParamChangePermission {
     pub params: Vec<ParamPermission>,
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema, Hash)]
+#[cw_serde]
+#[serde(rename_all = "snake_case")]
+#[derive(Eq, Hash)]
 pub struct ParamPermission {
     pub subspace: String,
     pub key: String,
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
+#[cw_serde]
 pub enum UpdateParamsPermission {
     CronUpdateParamsPermission(CronUpdateParamsPermission),
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
+#[cw_serde]
+#[serde(rename_all = "snake_case")]
 pub struct CronUpdateParamsPermission {
     pub security_address: bool,
     pub limit: bool,
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
+#[cw_serde]
+#[serde(rename_all = "snake_case")]
 pub struct CronPermission {
     pub add_schedule: bool,
     pub remove_schedule: bool,
