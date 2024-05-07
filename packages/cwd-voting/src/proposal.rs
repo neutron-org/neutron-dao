2df015b70b11be069deba0096dd41de99cc34348
swelf <golishkin@gmail.com>
2024-05-07 14:06:45 +0300

    30_000 -> 30_000_000

--- packages/cwd-voting/src/proposal.rs
+++ packages/cwd-voting/src/proposal.rs
@@ -4,7 +4,7 @@ use crate::status::Status;
 
 /// Default limit for proposal pagination.
 pub const DEFAULT_LIMIT: u64 = 30;
-pub const MAX_PROPOSAL_SIZE: u64 = 30_000;
+pub const MAX_PROPOSAL_SIZE: u64 = 30_000_000;
 
 pub trait Proposal {
     fn proposer(&self) -> Addr;
