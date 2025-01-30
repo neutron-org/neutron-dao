3760495f6605186cc67df8de753177bbfbc52794
nhpd <dmitry.klpv@gmail.com>
2025-01-30 19:02:46 +0400

    update_stake test

--- contracts/dao/neutron-staking-info-proxy/src/testing/tests.rs
+++ contracts/dao/neutron-staking-info-proxy/src/testing/tests.rs
@@ -1,14 +1,14 @@
 use crate::contract::{execute, instantiate};
+use crate::error::ContractError::Unauthorized;
 use crate::msg::{ExecuteMsg, InstantiateMsg};
 use crate::state::{CONFIG, PROVIDERS};
-use crate::testing::mock_querier::{
-    mock_dependencies, PROVIDER1, PROVIDER2, STAKING_REWARDS_CONTRACT,
-};
+use crate::testing::mock_querier::{mock_dependencies, STAKING_REWARDS_CONTRACT};
 use cosmwasm_std::testing::MockApi;
 use cosmwasm_std::{
     testing::{message_info, mock_env},
-    Addr, Order,
+    to_json_binary, Addr, Order, SubMsg, WasmMsg,
 };
+use neutron_staking_rewards::msg::ExecuteMsg::UpdateStake as RewardsMsgUpdateStake;
 
 // Helper to create a default instantiate message
 fn default_init_msg(api: MockApi) -> InstantiateMsg {
@@ -46,7 +46,7 @@ fn test_update_config() {
 
     let info2 = message_info(&deps.api.addr_make("stranger"), &[]);
     let res_2 = execute(deps.as_mut(), env.clone(), info2, update_msg.clone());
-    assert!(res_2.is_err());
+    assert_eq!(res_2.err().unwrap(), Unauthorized {});
 
     // Authorized update
     let res_3 = execute(deps.as_mut(), env.clone(), info.clone(), update_msg);
@@ -80,23 +80,87 @@ fn test_update_providers() {
 
     // Unauthorized update
     let update_msg = ExecuteMsg::UpdateProviders {
-        providers: vec![PROVIDER1.to_string(), PROVIDER2.to_string()],
+        providers: vec![
+            deps.api.addr_make("provider1").to_string(),
+            deps.api.addr_make("provider2").to_string(),
+        ],
     };
 
     let info2 = message_info(&deps.api.addr_make("stranger"), &[]);
     let res_2 = execute(deps.as_mut(), env.clone(), info2, update_msg.clone());
-    assert!(res_2.is_err());
+    assert_eq!(res_2.err().unwrap(), Unauthorized {});
 
     // Authorized update
     let res_3 = execute(deps.as_mut(), env.clone(), info.clone(), update_msg);
     assert!(res_3.is_ok());
 
     let providers: Vec<Addr> = PROVIDERS
-        .keys(&deps.storage, None, None, Order::Descending)
+        .keys(&deps.storage, None, None, Order::Ascending)
         .map(|k| k.unwrap())
         .collect();
     assert_eq!(
         providers,
-        vec![Addr::unchecked(PROVIDER1), Addr::unchecked(PROVIDER2)]
+        vec![
+            deps.api.addr_make("provider1"),
+            deps.api.addr_make("provider2")
+        ]
     );
 }
+
+/// Tests the following scenario:
+///     1.  A non-authorized address tries to proxy stake update (error)
+///     2.  An authorized address tries to proxy stake update (check correct RewardsMsgUpdateStake is created)
+#[test]
+fn test_update_stake() {
+    let mut deps = mock_dependencies();
+
+    // Instantiate
+    let env = mock_env();
+    let info = message_info(&deps.api.addr_make("owner"), &[]);
+    let msg = InstantiateMsg {
+        owner: deps.api.addr_make("owner").into(),
+        staking_rewards: Some(STAKING_REWARDS_CONTRACT.to_string()),
+        staking_denom: "untrn".to_string(),
+        providers: vec![
+            deps.api.addr_make("provider1").to_string(),
+            deps.api.addr_make("provider2").to_string(),
+        ],
+    };
+    let res = instantiate(deps.as_mut(), env.clone(), info.clone(), msg).unwrap();
+    assert_eq!(res.messages.len(), 0);
+
+    let stranger_provider_info = message_info(&deps.api.addr_make("stranger"), &[]);
+    let msg = ExecuteMsg::UpdateStake {
+        user: deps.api.addr_make("user1").into(),
+    };
+    let res = execute(
+        deps.as_mut(),
+        env.clone(),
+        stranger_provider_info,
+        msg.clone(),
+    );
+    assert_eq!(res.err().unwrap(), Unauthorized {});
+
+    let provider_info = message_info(&deps.api.addr_make("provider1"), &[]);
+    let res = execute(deps.as_mut(), env.clone(), provider_info, msg.clone());
+    assert!(res.is_ok());
+    let resp = res.unwrap();
+
+    let expected = WasmMsg::Execute {
+        contract_addr: STAKING_REWARDS_CONTRACT.to_string(),
+        msg: to_json_binary(&RewardsMsgUpdateStake {
+            user: deps.api.addr_make("user1").to_string(),
+        })
+        .unwrap(),
+        funds: vec![],
+    };
+    assert_eq!(resp.messages, vec![SubMsg::reply_never(expected)])
+}
+
+/// Tests the following scenario:
+///     1. Query with no providers set
+///     2. Query with one provider
+///     3. Query with multiple providers, some of them return non staking denom
+///     4. Query with multiple providers, some of them return error
+#[test]
+fn test_query_stake_query() {}
